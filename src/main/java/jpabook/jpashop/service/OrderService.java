33fcd07120652e7f5025592736c7b18f5cfb6dca
kang1027 <kang3171611@naver.com>
2023-05-02 13:26:23 +0900

    Web View

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -40,7 +40,7 @@ public class OrderService {
         OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
 
         //주문 생성
-        Order order = Order.createOrder(member, delivery, orderItem, orderItem);
+        Order order = Order.createOrder(member, delivery, orderItem);
 
         //주문 저장
         orderRepository.save(order);
